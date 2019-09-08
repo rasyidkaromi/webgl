2422f6ccabd9867066129d02b2ff5d51809a7a0a
Rasyid Karomi <waverate@gmail.com>
2019-09-09 00:11:00 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	// "sync"
+
+	"github.com/zserge/lorca"
+)
+
+
+
+func main() {
+	args := []string{}
+	if runtime.GOOS == "linux" {
+		args = append(args, "--class=Lorca")
+	}
+	ui, err := lorca.New("", "", 480, 320, args...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+
+	// A simple way to know when UI is ready (uses body.onload event in JS)
+	ui.Bind("start", func() {
+		log.Println("UI is ready")
+	})
+
+	// Load HTML.
+	// You may also use `data:text/html,<base64>` approach to load initial HTML,
+	// e.g: ui.Load("data:text/html," + url.PathEscape(html))
+
+	ln, err := net.Listen("tcp", "127.0.0.1:9999")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(FS))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
+
+	// You may use console.log to debug your JS code, it will be printed via
+	// log.Println(). Also exceptions are printed in a similar manner.
+	ui.Eval(`
+		console.log("Hello, world!");
+		console.log('Multiple values:', [1, false, {"x":5}]);
+	`)
+
+	// Wait until the interrupt signal arrives or browser window is closed
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+	select {
+	case <-sigc:
+	case <-ui.Done():
+	}
+
+	log.Println("exiting...")
+}
